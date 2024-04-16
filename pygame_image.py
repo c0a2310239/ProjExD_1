92dc5f4a717b6117a9a07970ee787344b31cde1c
tbt <c0a2310239@edu.teu.ac.jp>
2024-04-16 16:45:28 +0900

    動きを滑らかに

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)  #練習7-1
     koukaton_img = pg.image.load("fig/3.png")  #練習2
     koukaton_img = pg.transform.flip(koukaton_img,True,False)
     tmr = 0
@@ -17,12 +18,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr % 800
-        screen.blit(bg_img, [-x, 0])
+        x = tmr % 3200    
+        screen.blit(bg_img, [-x, 0])   #練習6
+        screen.blit(bg_img2,[-x+1600,0])  #練習7-1
+        screen.blit(bg_img, [-x+3200, 0])  #練習7-2
+        screen.blit(bg_img2,[-x+4800,0])  #練習7-2
         screen.blit(koukaton_img, [300, 200])  #練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200)   #練習6
+
+
 
 
 if __name__ == "__main__":
