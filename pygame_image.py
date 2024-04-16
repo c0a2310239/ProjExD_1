e05b6f6515423198c31d48d8f1670ae026475e02
tbt <c0a2310239@edu.teu.ac.jp>
2024-04-16 16:31:44 +0900

    歪みをなくす

--- pygame_image.py
+++ pygame_image.py
@@ -17,8 +17,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(koukaton_img, [300, 200])
+        x = tmr % 800
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(koukaton_img, [300, 200])  #練習4
         pg.display.update()
         tmr += 1        
         clock.tick(200)
